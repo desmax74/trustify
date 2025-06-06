b3afcfab17e8fc61392aa00ae0e8975ed23a5d37
github-action-benchmark <github@users.noreply.github.com>
2025-06-06 15:52:27 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 93df8ff9958e4dea60bbb52a90b84e139e44a75a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744022986602,
-  "repoUrl": "https://github.com/trustification/trustify",
+  "lastUpdate": 1749225147759,
+  "repoUrl": "https://github.com/desmax74/trustify",
   "entries": {
     "Benchmark": [
       {
@@ -8230,6 +8230,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/trustification/trustify/commit/8f150ceefc2008a985ab2af90d3a4624cb9d8a32"
         },
         "date": 1744022985829,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 8,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "committer": {
+            "email": "00hf11@gmail.com",
+            "name": "Helio Frota",
+            "username": "helio-frota"
+          },
+          "distinct": true,
+          "id": "93df8ff9958e4dea60bbb52a90b84e139e44a75a",
+          "message": "test(importer): increase unit test coverage and refactor test helpers\n\nAdd unit tests for Importer methods: is_due, is_running, is_enabled.\n\nAssisted-by: Cursor",
+          "timestamp": "2025-06-06T11:39:59Z",
+          "tree_id": "639cc9f8a0413618c5c68d0a7218681bc4b4896e",
+          "url": "https://github.com/desmax74/trustify/commit/93df8ff9958e4dea60bbb52a90b84e139e44a75a"
+        },
+        "date": 1749225146823,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
